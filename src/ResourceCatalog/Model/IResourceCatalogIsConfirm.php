650c6fd96c4c2c41598be843818d4404e3021e94
Marhal <junwuji555@sina.com>
2020-10-28 16:55:46 +0800

    同步资源目格式

---
+++ src/ResourceCatalog/Model/IResourceCatalogIsConfirm.php
@@ -0,0 +1,27 @@
+<?php
+namespace ResourceCatalog\Model;
+
+interface IResourceCatalogIsConfirm
+{
+    const TYPE_RESOURECATALOG_CONFIRM
+        = array(
+            IResourceCatalog::TYPE['NULL'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['FRJQTZZSXFB'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['ZRRSXFB'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['BWMXWCFJLGAJ'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['BWMXWCFJLZJWZ'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['BWMXWCFJLGAJJT'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['BWMXWCFJLMZJWMBZ'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['BWMXWCFJLZJJWMJY'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['BWMXWCFJLQWLJWMLY'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['TJQYFRGGXYZHPJJG'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['QYYCMLXX'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['QIYEJBXX'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['XBXZCF'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['XBXZXK'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+
+            IResourceCatalog::TYPE['XZCF'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['XZXK'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL'],
+            IResourceCatalog::TYPE['TEST'] => ISearchableResourceCatalogData::TYPE_CONFIRM['NULL']
+        );
+}
---
+++ src/ResourceCatalog/Model/IUserGroupIdentify.php
@@ -0,0 +1,19 @@
+<?php
+namespace ResourceCatalog\Model;
+
+interface IUserGroupIdentify
+{
+    const IDENTIFY = array(
+        IUserGroup::ID['NULL'] => 'NULL',
+        IUserGroup::ID['FGW'] => 'QFGW',
+        IUserGroup::ID['GAJ'] => 'QWWXB',
+        IUserGroup::ID['XZSPJ'] => 'RMTZX',
+        IUserGroup::ID['QWLJ'] => 'SWJ',
+        IUserGroup::ID['GTJ'] => 'ZJW',
+        IUserGroup::ID['ZJWZ'] => 'WJW',
+        IUserGroup::ID['ZJJ'] => 'NW',
+        IUserGroup::ID['MZJ'] => 'GZW',
+        IUserGroup::ID['XQZD'] => 'KJJ',
+        IUserGroup::ID['RMFY'] => 'GXJ',
+    );
+}
